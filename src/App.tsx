8633519de2d8e16e8a1e09dbf8687aaa9e07f807
yuxuetr <54.yeying@gmail.com>
2025-04-04 14:51:15 +0800

    update: tailwindcss 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
 function HelloReact() {
   return (
-    <div className="tw-flex tw-justify-center tw-items-center tw-h-screen tw-bg-primary-content">
-      <div className="tw-card tw-w-96 tw-bg-primary place-content-center tw-text-primary-content">
-        <div className="tw-card-body">
-          <h2 className="tw-card-title">React + Tailwind CSS + DaisyUI</h2>
-          <p>Initalize a project with React + Tailwind CSS + DaisyUI</p>
-          <div className="tw-card-actions tw-justify-end">
-            <button className="tw-btn">Get Started</button>
-          </div>
-        </div>
-      </div>
+    <div className="flex justify-center items-center h-screen bg-primary-content">
+      <h1 className="text-4xl font-bold">Hello React</h1>
     </div>
   )
 }
